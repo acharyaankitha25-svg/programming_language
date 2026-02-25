4074356d9e8ee71ffe7307f96963235d96322c31
acharyaankitha25-svg <acharyaankitha25@gmail.com>
2026-02-25 09:45:33 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class sum
+{
+	public static voidmain(string args[])
+	{
+		Scanner new=Scanner Scanner(system.in);
+		System.out.println("enetr a number");
+		int a=S.nextInt();
+		System.out.println("entr number");
+		int b=S.nextInt();
+		int sum=a+b;
+		System.out.println("sum="+sum);
+      }
+}
